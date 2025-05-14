3ad4f3df3e89e8d356e8ee3dbd4a0dd7719ed26e
sudarshan raveendranath dayarathna <120754357+sudarshan-raveendranath@users.noreply.github.com>
2025-05-14 20:27:48 +0530

    update the payment service

--- payment-service/src/main/java/com/sudarshan/paymentservice/dto/PaymentRequest.java
+++ payment-service/src/main/java/com/sudarshan/paymentservice/dto/PaymentRequest.java
@@ -1,10 +1,11 @@
 package com.sudarshan.paymentservice.dto;
 
 
-import com.sudarshan.paymentservice.enums.Currency;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 
 @Data
@@ -12,16 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PaymentRequest {
 
-    private Long orderId;
     private Long restaurantId;
-    private Long riderId;
-
-    private Long totalAmount;       // This includes mealPrice + deliveryCharge
-    private Long mealPrice;
+    private Long totalAmount;
     private Long deliveryCharge;
     private Integer numberOfMeals;
-
-    private String mealName;
-
-    private Currency currency;
+    private List<String> mealNames;
 }
